65416cdad8116257c3876b564dfd87363471e59b
Aleksei Ponomarev <kothulit@gmail.com>
2025-09-10 14:35:14 +0500

    Создан проект

---
+++ OxyzWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OxyzWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
