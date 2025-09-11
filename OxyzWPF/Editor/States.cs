5d3935233445f8e397e4dcafccd30fc9abfa06bb
Aleksei Ponomarev <kothulit@gmail.com>
2025-09-11 12:31:43 +0500

    Добавлены классы состояния

---
+++ OxyzWPF/Editor/States.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace OxyzWPF.Editor;
+
+public class NavigationState : IEditorState
+{
+    public void Enter() => Debug.WriteLine("Navigation mode ON");
+    public void Exit() => Debug.WriteLine("Navigation mode OFF");
+    public void Update(double deltaTime) { /* Здесь можно обновлять камеру */ }
+    public void OnMouseClick(double x, double y)
+    {
+        Debug.WriteLine($"[Navigation] Click at {x},{y}");
+    }
+}
+
+public class EditState : IEditorState
+{
+    public void Enter() => Debug.WriteLine("Edit mode ON");
+    public void Exit() => Debug.WriteLine("Edit mode OFF");
+    public void Update(double deltaTime) { /* Здесь обновляем выделенные объекты */ }
+    public void OnMouseClick(double x, double y)
+    {
+        Debug.WriteLine($"[Edit] Click at {x},{y}");
+    }
+}
