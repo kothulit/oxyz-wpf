f48d1fa0fcff86f363a3a9cd4446eca22cf2bde5
kothulit <kothulit@gmail.com>
2025-10-16 11:41:46 +0500

    Добавил заготовки для компонента и системы выбора элементов

---
+++ OxyzWPF/ECS/Components/SelectionComponent.cs
@@ -0,0 +1,8 @@
+﻿using OxyzWPF.Contracts.ECS;
+
+namespace OxyzWPF.ECS.Components; 
+
+internal class SelectionComponent : IComponent
+{
+    public bool IsSelected = false;
+}
---
+++ OxyzWPF/ECS/Systems/SelectionSystem.cs
@@ -0,0 +1,24 @@
+﻿using HelixToolkit.Wpf.SharpDX;
+using OxyzWPF.Contracts.ECS;
+using OxyzWPF.Contracts.Mailing;
+
+namespace OxyzWPF.ECS.Systems;
+
+internal class SelectionSystem : ISystem
+{
+    private readonly IWorld _world;
+    private readonly IMailer _mailer;
+    private List<int> _selectedObjectIds;
+
+    public SelectionSystem(IWorld world, IMailer mailer)
+    {
+        _world = world;
+        _mailer = mailer;
+        _selectedObjectIds = new List<int>();
+    }
+
+    public void Update(double deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+}
