2190a12a27feb408f3d7477c775cb4b33f2b8863
Aleksei Ponomarev <kothulit@gmail.com>
2025-11-26 19:48:15 +0500

    Сохранил файл множества для геометрии

--- OxyzWPF/Contracts/ECS/GeometryEnum.cs
+++ OxyzWPF/Contracts/ECS/GeometryEnum.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace OxyzWPF.Contracts.ECS;
 
-namespace OxyzWPF.Contracts.ECS
+public enum GeometryEnum
 {
-    internal class GeometryEnum
-    {
-    }
+    None = 0,
+    Vertex = 11,
+    Edge = 12,
+    Face = 13,
+    BoundingBox = 15,
+    Extrusoin = 21,
+    Blend = 22,
+    Sweep = 23,
+    Point = 31,
+    Quad = 32,
+    Box = 32,
+    Plane = 33,
+    Sphere = 33,
+    Cylinder = 34,
+    Arrow = 35,
+    Pipe = 36,
 }
